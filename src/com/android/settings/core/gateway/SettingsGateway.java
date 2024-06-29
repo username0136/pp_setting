yaap/packages_apps_Settings

Settings: Declare an activity for WirelessDebuggingFragment
we want the debugging is connected notification to open this page instead of just dev settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -87,6 +87,7 @@ import com.android.settings.datausage.DataUsageSummary;
 import com.android.settings.datetime.DateTimeSettings;
 import com.android.settings.deletionhelper.AutomaticStorageManagerSettings;
 import com.android.settings.development.DevelopmentSettingsDashboardFragment;
+import com.android.settings.development.WirelessDebuggingFragment;
 import com.android.settings.deviceinfo.PrivateVolumeForget;
 import com.android.settings.deviceinfo.PublicVolumeSettings;
 import com.android.settings.deviceinfo.StorageDashboardFragment;
@@ -351,6 +352,7 @@ public class SettingsGateway {
             TurnScreenOnDetails.class.getName(),
             GamingModeFragment.class.getName(),
             RunningServices.class.getName(),
+            WirelessDebuggingFragment.class.getName(),
             YaapSettings.class.getName(),
             WifiTetherClientManager.class.getName()
     };
@@ -400,5 +402,6 @@ public class SettingsGateway {
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.YAAPSettingsActivity.class.getName(),
             Settings.PreviouslyConnectedDeviceDashboardActivity.class.getName(),
+            Settings.WirelessDebuggingActivity.class.getName(),
     };
 }

username0136/pp_setting

Change-Id: I30e4eb3c4519e421afd6bcf1d10f35b451baf1c1

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -95,6 +95,7 @@ import com.android.settings.datausage.DataUsageSummary;
 import com.android.settings.datetime.DateTimeSettings;
 import com.android.settings.deletionhelper.AutomaticStorageManagerSettings;
 import com.android.settings.development.DevelopmentSettingsDashboardFragment;
+import com.android.settings.development.WirelessDebuggingFragment;
 import com.android.settings.deviceinfo.PrivateVolumeForget;
 import com.android.settings.deviceinfo.PublicVolumeSettings;
 import com.android.settings.deviceinfo.StorageDashboardFragment;
@@ -390,7 +391,8 @@ public class SettingsGateway {
             ScreenTimeoutSettings.class.getName(),
             ResetNetwork.class.getName(),
             VibrationIntensitySettingsFragment.class.getName(),
-            LiveDisplaySettings.class.getName()
+            LiveDisplaySettings.class.getName(),
+            WirelessDebuggingFragment.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -437,5 +439,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.WirelessDebuggingActivity.class.getName(),
     };
 }